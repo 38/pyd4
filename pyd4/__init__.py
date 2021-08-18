163ae6a7bcd4b7a0141cbf089c86c59c599b30ce
Hao Hou <haohou302@gmail.com>
2021-08-18 16:40:53 -0600

    add enumerate API

--- pyd4/__init__.py
+++ pyd4/__init__.py
@@ -4,4 +4,7 @@ The Python Binding for the D4 file format
 
 from .pyd4 import D4File, D4Iter
 
+def enumerate_values(inf, chrom, begin, end):
+    return map(lambda p: (chrom, p[0], p[1]), zip(range(begin, end), inf.value_iter(chrom, begin, end)))
+
 __all__ = [ 'D4File', 'D4Iter' ]
